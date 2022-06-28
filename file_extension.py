2dcba7a7ef7ecd79161bb79de7ac99af47630db1
Siddhi Yevale <siddhiyevale7800@gmail.com>
2022-06-28 09:46:00 +0530

    python assigment done

--- file_extension.py
+++ file_extension.py
@@ -1,8 +1,4 @@
 filename = input("Input the Filename: ")
+fileExtList = {"py": "python", "java": "Java"}
 file_arr = filename.split(".")
-file_extension = ''
-if(file_arr[-1] == 'py'):  # checking the file extension
-    file_extension = 'python'
-else:
-    file_extension = file_arr[-1]
-print("The extension of the file is : " + repr(file_extension))
+print("The extension of the file is : " + fileExtList[file_arr[-1]])
